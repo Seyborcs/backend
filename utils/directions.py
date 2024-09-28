3b82cd33aa2b12ef2f9d3d656088b5db6e1c2952
Tosiekdev <antonizieciak@gmail.com>
2024-09-28 19:24:57 +0200

    Directions

---
+++ utils/directions.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Directions(Enum):
+    SOUTH = 1
+    WEST_SOUTH = 2
+    WEST = 3
+    WEST_NORTH = 4
+    NORTH = 5
+    EAST_NORTH = 6
+    EAST = 7
+    EAST_SOUTH = 8
+    NONE = 9
