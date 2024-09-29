6b108b54140e9857380eb1824e7951f20c9dc25c
Tosiekdev <antonizieciak@gmail.com>
2024-09-29 03:24:26 +0200

    status codes for adding event

--- routers/events.py
+++ routers/events.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 import time
 
@@ -18,8 +18,10 @@ router = APIRouter()
 def return_events(lon: float, lat: float):
     return db.search_proximity(Point.create(lon, lat))
 
-@router.post("/")
+@router.post("/", status_code=201)
 def add_event(event: EventForm):
+    if event.kind not in db.USER_ADDED and event.kind not in db.MALOPOLSKA_ADDED:
+        raise HTTPException(status_code=400, detail="Unknown kind!")
     t = time.time()
 
     e = Event(event.lon, event.lat, event.kind, t)
